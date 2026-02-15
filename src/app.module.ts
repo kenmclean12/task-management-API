67071aad2501e0f2cb26a5f3919b06533d6c61e4
kenmclean12 <kenmclean20@gmail.com>
2026-02-14 22:41:51 -0800

    quick fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { UserModule } from './user/user.module';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { ClientModule } from './client/client.module';
 import { AddressModule } from './address/address.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, ClientModule, AddressModule],
+  imports: [PrismaModule, UserModule, ClientModule, AddressModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, JwtAuthGuard],
 })
