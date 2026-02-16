e213de765dc3f5d9ff1c3766cb00dfa871765a73
kenmclean12 <kenmclean20@gmail.com>
2026-02-15 18:37:51 -0800

    quick fix

--- src/job/job.module.ts
+++ src/job/job.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { JobService } from './job.service';
+import { JobController } from './job.controller';
 
 @Module({
   imports: [PrismaModule],
-  exports: [],
-  controllers: [],
+  exports: [JobService],
+  controllers: [JobController],
   providers: [JobService],
 })
 export class JobModule {}
